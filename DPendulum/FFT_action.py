bfee6d87c465bf78201ae1ec8e056c4ff3b1aad8
Zhuoyue-Huang <bestyhuang35@gmail.com>
2022-06-02 18:05:16 +0100

    an approx periodic case

--- DPendulum/FFT_action.py
+++ DPendulum/FFT_action.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy import pi
 
 # FFT plot
-y0 = [pi/10, 0, -pi/50, 0]
+y0 = [pi/12, 0, np.sqrt(2)*pi/12, 0]
 pendulum = Pendulum(theta1=y0[0], z1=y0[1], theta2=y0[2], z2=y0[3], tmax=25, dt=0.05, y0=y0)
 pendulum.fft_plot()
 
@@ -16,8 +16,8 @@ omega1_pval, omega1_pind, omega2_pval, omega2_pind = pendulum.find_peak()
 plt.figure(figsize = (15, 6))
 plt.subplot(121)
 plt.plot(t, theta1)
-plt.plot(t, (np.cos(omega1_pval[0]*t)+np.cos(omega1_pval[1]*t))/4)
+plt.plot(t, np.cos(omega1_pval[0]*t)/3.5)
 plt.subplot(122)
 plt.plot(t, theta2)
-plt.plot(t, (np.sin(omega2_pval[0]*t)+np.sin(omega2_pval[1]*t))/4)
+plt.plot(t, np.sin(omega2_pval[0]*t)/3)
 plt.show()
